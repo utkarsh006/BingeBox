bbfe950bbcaa1656ecd661198a37565ec826c8da
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2025-12-05 14:51:57 +0530

    add number infront of each card

--- vite-project/src/components/MoviesList.tsx
+++ vite-project/src/components/MoviesList.tsx
@@ -5,8 +5,12 @@ const MovieList = ({ movies }: MovieListProps) => {
     return (
         <div className="relative">
             <ul className="flex overflow-x-scroll overflow-y-visible space-x-4 px-4 md:px-6 py-4 scrollbar-hide relative">
-                {movies.map((movie: Movie) => (
-                    <li key={movie.id}>
+                {movies.map((movie: Movie, index: number) => (
+                    <li
+                        key={movie.id}
+                        className="movie-card-item"
+                        data-index={index + 1}
+                    >
                         <MovieCard movie={movie} />
                     </li>
                 ))}
